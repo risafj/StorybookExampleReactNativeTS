73b1aa12a4647006b82e852ec6a30d115c90592e
Risa Fujii <risaf1231@gmail.com>
2021-01-06 08:55:11 +0900

    Configure ESLint rules and .eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,44 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community/eslint-config',
+    'standard-with-typescript',
+    'eslint-config-prettier'
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-};
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-native'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'project': './tsconfig.json'
+  },
+  'env': {
+    'react-native/react-native': true
+  },
+  'rules': {
+    'prettier/prettier': 'off',
+    'react-native/no-unused-styles': 'warn',
+    'react-native/no-inline-styles': 'error',
+    'react-native/no-raw-text': ['warn', {
+      skip: ['CustomText']
+    }],
+    'react-native/no-single-element-style-arrays': 'warn',
+    'object-curly-spacing': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/jsx-curly-spacing': ['error', {
+      when: 'always',
+      allowMultiline: true,
+      children: true
+    }],
+    'eol-last': ['error', 'always'],
+    'no-multiple-empty-lines': 'error',
+  }
+}
