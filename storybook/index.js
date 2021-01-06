4cb3de2369944493fe491945ed61e55b58db92e4
Risa Fujii <risaf1231@gmail.com>
2021-01-06 21:23:29 +0900

    Add and configure React Native Storybook Loader
    https://github.com/elderfo/react-native-storybook-loader#quick-start

--- storybook/index.js
+++ storybook/index.js
@@ -4,6 +4,8 @@ import { AppRegistry, Platform } from 'react-native';
 import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
 import { withKnobs } from '@storybook/addon-knobs';
 
+import { loadStories } from './storyLoader';
+
 import './rn-addons';
 
 // enables knobs for all stories
@@ -11,7 +13,7 @@ addDecorator(withKnobs);
 
 // import stories
 configure(() => {
-  require('./stories');
+  loadStories()
 }, module);
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
