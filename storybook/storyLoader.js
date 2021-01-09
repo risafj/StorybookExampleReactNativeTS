ca799a86c3e5442aab68e7bd20ced224bc91211a
Risa Fujii <risaf1231@gmail.com>
2021-01-09 15:02:41 +0900

    Add CustomButton stories

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -4,10 +4,14 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
+  require('../src/components/atoms/CustomButton.stories');
   require('../src/components/atoms/Gravatar.stories');
 }
 
-const stories = ['../src/components/atoms/Gravatar.stories'];
+const stories = [
+  '../src/components/atoms/CustomButton.stories',
+  '../src/components/atoms/Gravatar.stories',
+];
 
 module.exports = {
   loadStories,
