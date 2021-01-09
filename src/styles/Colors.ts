1f8e8fd3f89b0965bdef7378a80ae90daf1c9774
Risa Fujii <risaf1231@gmail.com>
2021-01-09 14:17:31 +0900

    Add Gravatar component

---
+++ src/styles/Colors.ts
@@ -0,0 +1,13 @@
+export default {
+  text: 'black',
+  textGray: '#7a7a7a',
+  background: '#F6F6F6',
+  backgroundGray: '#D4D4D4',
+  primary: '#F3724F',
+  primaryStrong: '#AE3027',
+  card: '#FEEDDC',
+  complementary: '#14BEE0',
+  error: '#F23548',
+  success: '#7ABC36',
+  selected: '#FFBF00'
+}
